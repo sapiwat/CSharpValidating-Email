8fe30cd99f5b54c6066c2107e31e1b184107d3f3
Apiwat Sangthong <sapiwat@gmail.com>
2026-02-07 00:32:33 +1000

    add alternative method to solve the problem

--- Program.cs
+++ Program.cs
@@ -27,14 +27,16 @@ class Program
             return;
         }
         //check if there is "@" symbol
-        if (email.IndexOf('@') == -1)
+        if (email.IndexOf('@') == -1) //can use email.Contains('@')
         {
             Console.WriteLine("Emails must contain \"@\"");
             return;
         }
 
         //check if it ends with ".com"
-        if (email.ToLower().LastIndexOf(".com") != email.Length - 4) //4 is the lenghth of ".com"
+        if (email.ToLower().LastIndexOf(".com") != email.Length - 4)
+        //4 is the lenghth of ".com"
+        //can also use substring method to cut the last 4 characters to see if it is ".com"
         {
             Console.WriteLine("Emails must end with .com");
             return;
